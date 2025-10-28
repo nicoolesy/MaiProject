691238bb3e17f6f6421b12b317ae1c57732910f7
Nicole Chen <nicole.chen@usequantum.com>
2025-10-28 17:45:20 -0400

    Avalible gemini

--- ai_parenting_assis.py
+++ ai_parenting_assis.py
@@ -19,7 +19,13 @@ import time
 import google.api_core.exceptions as google_exceptions
 from dotenv import load_dotenv
 
+
 load_dotenv()
+# Access the API key
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+
+genai.configure(api_key=GOOGLE_API_KEY)
+model = genai.GenerativeModel("gemini-1.5-flash")
 
 with open(f"parenting_knowledge_base_20_clean_en.json", "r") as f:
     data = json.load(f)
