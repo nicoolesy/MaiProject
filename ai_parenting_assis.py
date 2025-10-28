836c8de3e3ae4e1480c6aa6434c541fb7f839c6f
Nicole Chen <nicole.chen@usequantum.com>
2025-10-27 20:25:04 -0400

    Change Gemini model

--- ai_parenting_assis.py
+++ ai_parenting_assis.py
@@ -34,7 +34,7 @@ load_dotenv()
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 
 genai.configure(api_key=GOOGLE_API_KEY)
-model = genai.GenerativeModel("gemini-2.0-flash-lite")
+model = genai.GenerativeModel("gemini-1.5-flash")
 
 with open(f"parenting_knowledge_base_20_clean_en.json", "r") as f:
     data = json.load(f)
